2655b03323259e30f51bd37a9f075e414e14ad86
Waseem Ahmed <alphawaseem@gmail.com>
2016-08-25 14:38:46 +0530

    Create custom earthquake class called Quake

---
+++ app/src/main/java/com/example/android/quakereport/Quake.java
@@ -0,0 +1,60 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by waseem on 8/25/16.
+ * Quake class is used to store information about the
+ * earthquake that have took place. Quake object contains
+ * 3 information
+ *  1. the magnitude of the earthquake
+ *  2. the city in which earthquake took place
+ *  3. the date
+ *
+ */
+public class Quake {
+
+    private int magnitude ; // magnitude of the earthquake
+    private String city ;  // city of earthquake
+    private String date ; // date of earthquake
+
+    /**
+     * Create new Quake object
+     *
+     *
+     *
+     * @param magnitude is the magnitude of earthquake (e.g. 4.7)
+     * @param city is the city in which earthquake took place (e.g. Tokyo)
+     * @param date is the date on which earthquake took place (e.g. Feb 2,2016)
+     *
+     */
+    public Quake(int magnitude , String city , String date) {
+
+        this.magnitude = magnitude ;
+        this.city = city;
+        this.date = date ;
+    }
+
+    /**
+     *
+     * @return the magnitude of earthquake
+     */
+    public int getMagnitude() {
+        return magnitude ;
+    }
+
+    /**
+     *
+     * @return the city of earthquake
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     *
+     * @return the date of earthquake ( MM DD,YYYY)
+     */
+    public String getDate() {
+        return date;
+    }
+
+}
